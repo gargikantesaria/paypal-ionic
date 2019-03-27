8be69357733ab1e9b02985fc2b0fb317d680120e
gargikantesaria <gargi.kantesaria@solutionanalysts.com>
2019-03-27 08:32:10 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+declare var paypal: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  paymentAmount = 200;
+  payPalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AZfa5sAt8gFQiJv-dJBsdTb81dAQzunBEtK6B76AARf4YXal8DCkD5QcWJSrJt7v0iaSxIVw7TF01DFo',
+    },
+    commit: false,
+    payment: (data, actions) => {
+      console.log("data is", data, actions);
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.paymentAmount, currency: 'CAD' } }
+          ]
+        }
+      });
+    }
   }
+  constructor(public navCtrl: NavController) {}
 
+  payButtonHandler() {
+    console.log(paypal);
+    paypal.Buttons(this.payPalConfig).render('#paypal-button');
+  }
 }
